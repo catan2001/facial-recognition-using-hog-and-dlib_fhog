91dbeed06aabe2600cb03f91d6a6facc0cccacb0
mylcaa <milicapuhalo@gmail.com>
2024-03-11 16:31:45 +0100

    file debugging - hog.c

--- hog.c
+++ hog.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 //assume the size of our picture is fixed and already determined
 #define ROWS 200
-#define COLS 200
+#define COLS 600
 
 #define CELL_SIZE 8
 #define nBINS 6
@@ -15,12 +16,10 @@
 const char filter_x[9] = {1,0,-1, 2,0,-2, 1,0,-1};  //Dx = filter_x conv D
 const char filter_y[9] = {1,2,1, 0,0,0, -1,-2,-1};    //Dy = filter_y conv D
 
-//TODO: in main before calling this function you need to pad the image on all edges by 1 and fill them with zeros
-//so the filtered image has the same rezolution as the original
-void filter_image(int img[ROWS][COLS], int im_filtered[ROWS][COLS], const char* filter){
+void filter_image(float img[ROWS][COLS], float im_filtered[ROWS][COLS], const char* filter){
 
     //create a local matrix that will hold the padded image:
-    int padded_img[ROWS+2][COLS+2];
+    float padded_img[ROWS+2][COLS+2];
     
     //pad the image on the outer edges with zeros:
     for(int i=0; i<ROWS+2; ++i) {
@@ -43,7 +42,7 @@ void filter_image(int img[ROWS][COLS], int im_filtered[ROWS][COLS], const char* 
     }
 
     //filter image:
-    int imROI[9];
+    float imROI[9];
 
     for (int i=0; i<ROWS; ++i){
         for (int j=0; j<COLS; ++j){
@@ -59,7 +58,7 @@ void filter_image(int img[ROWS][COLS], int im_filtered[ROWS][COLS], const char* 
 }
 
 //getting the amplitude and phase matrix of the filtered image
-void get_gradient(int im_dx[ROWS][COLS], int im_dy[ROWS][COLS], float grad_mag[ROWS][COLS], float grad_angle[ROWS][COLS]){
+void get_gradient(float im_dx[ROWS][COLS], float im_dy[ROWS][COLS], float grad_mag[ROWS][COLS], float grad_angle[ROWS][COLS]){
     
     for(int i=0; i<ROWS; ++i){
         for(int j=0; j<COLS; ++j){
@@ -71,7 +70,7 @@ void get_gradient(int im_dx[ROWS][COLS], int im_dy[ROWS][COLS], float grad_mag[R
 
             //determining the phase matrix:
             if(abs(dX)>0.00001){
-                grad_angle[i][j] = atan(dY/dX) + PI/2;
+                grad_angle[i][j] = atan((float)dY/dX) + PI/2;
             }else if(dY<0 && dX<0){
                 grad_angle[i][j] = 0;
             }else{
@@ -133,23 +132,111 @@ void build_histogram(float grad_mag[ROWS][COLS], float grad_angle[ROWS][COLS], f
 
 int main(){
 
-    /*FILE * out;
-    out = fopen("color_test.ppm", "wb");
+    FILE * rach;
+    rach = fopen("dx.ppm", "rb");
 
-    fprintf(out, "P6 256 256 255\n");
-    
-    for(int r=0; r<256; r++) {
-        for(int b=0; b<256; b++) {
-            fputc(r, out);
-            fputc(0, out);
-            fputc(b, out);
+    unsigned char ch;
+    unsigned char a;
+    unsigned char matrix[ROWS][COLS], dx[ROWS][COLS];
+
+
+    printf("\n");
+
+    for(int k=0; k<15; ++k) ch = fgetc(rach); //predji na prvi clan niza
+
+    for (int i=0; i<ROWS; ++i){
+        for (int j=0; j<COLS; ++j){
+            
+            //ch = fgetc(rach);
+            //ch = fgetc(rach);
+            ch = fgetc(rach);
+            a=ch;
+
+            matrix[i][j] = a;
+            printf("%d ", (unsigned int)matrix[i][j]);
+        }
+        printf("\n");
+    }     
+
+     //printf("%d ", (int)matrix[1][0]);
+
+    fclose(rach);
+
+    /*FILE* dx, *dc;
+    dx = fopen("dx.ppm", "rb");
+    dc = fopen("dx_C.ppm", "rb");
+
+    char c, x;
+    int a=0;
+
+    do{
+        c = fgetc(dc);
+        x = fgetc(dx);
+        a++;
+
+        if(c != x){
+            printf("c=%d ", c);
+            printf("x=%d ", x);
+            printf("redni broj=%d \n", a);
+        }
+
+
+    }while(c != EOF);*/
+
+    /*FILE * rach;
+    rach = fopen("gray_rachel.ppm", "rb");
+
+    char ch;
+    float a;
+    float matrix[ROWS][COLS], dx[ROWS][COLS];
+
+
+    printf("\n");
+
+    for(int k=0; k<15; ++k) ch = fgetc(rach); //predji na prvi clan niza
+
+    for (int i=0; i<ROWS; ++i){
+        for (int j=0; j<COLS; ++j){
+            
+            //ch = fgetc(rach);
+            //ch = fgetc(rach);
+            ch = fgetc(rach);
+            a=ch;
+
+            matrix[i][j] = a;
+            //printf("%d ", (int)matrix[i][j]);
+        }
+        //printf("\n");
+    }      
+
+    fclose(rach);
+
+    filter_image(matrix, dx, filter_x);
+
+    printf("\n\n\n");
+
+    FILE * dx_rach;
+    dx_rach = fopen("dx_C.ppm", "wb");
+
+    fprintf(dx_rach, "P6 200 200 255\n");
+  
+    for(int i=0; i<200; i++) {
+        for(int j=0; j<600; j++) {
+        fputc((int)dx[i][j], dx_rach);
         }
     }
-  
-    fclose(out);*/
+    fclose(dx_rach);
 
-    FILE * rach;
-    rach = fopen("disgustedrachel.ppm", "r");
 
-  return 0;
+    for (int i=0; i<ROWS; ++i){
+        for (int j=0; j<COLS; ++j){
+        
+            printf("%d ", (int)dx[i][j]);
+        }
+        break;
+        printf("\n");
+    }  */
+
+
+    return 0;
 }
