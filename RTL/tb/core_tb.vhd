58f213003246640280df6eb6285d549b639919e2
catan2001 <nikolacatic2001@gmail.com>
2024-08-14 10:15:53 +0200

    updated core_tb, works...

--- RTL/tb/core_tb.vhd
+++ RTL/tb/core_tb.vhd
@@ -6,6 +6,7 @@ use std.textio.all;
 entity core_tb is
     generic (
         WIDTH: natural := 16; --input is [-2,2] -> 3.15
+        NUM_PIX : natural := 24;
         WIDTH_CONST: natural := 3
        );
 end core_tb;
@@ -45,12 +46,6 @@ generic (
         pix_21: in std_logic_vector(WIDTH - 1 downto 0);
         pix_22: in std_logic_vector(WIDTH - 1 downto 0);
         pix_23: in std_logic_vector(WIDTH - 1 downto 0); 
-        pix_24: in std_logic_vector(WIDTH - 1 downto 0);
-        pix_25: in std_logic_vector(WIDTH - 1 downto 0);
-        pix_26: in std_logic_vector(WIDTH - 1 downto 0);
-        pix_27: in std_logic_vector(WIDTH - 1 downto 0);
-        pix_28: in std_logic_vector(WIDTH - 1 downto 0);
-        pix_29: in std_logic_vector(WIDTH - 1 downto 0); 
 
         ------------------- output signals -------------------    
         res_x_0: out std_logic_vector(WIDTH - 1 downto 0);
@@ -75,8 +70,8 @@ end component core_top;
 
 signal clk_s: std_logic;
 
-type mem30 is array(0 to 29) of std_logic_vector(15 downto 0);
-signal reg30: mem30;
+type mem24 is array(0 to NUM_PIX-1) of std_logic_vector(15 downto 0);
+signal reg24: mem24;
 
 type mem16 is array(0 to 15) of std_logic_vector(15 downto 0);
 signal dram: mem16;
@@ -94,7 +89,7 @@ impure function bram_init return bram_type is
     variable data : std_logic_vector(WIDTH - 1 downto 0);
     variable bram_rows : rows_type;
     variable bram : bram_type;
-    file text_file : text open read_mode is "C:\Users\Andjela\Desktop\RTL\tb\input_file\gray_normalised.txt";
+    file text_file : text open read_mode is "C:\Users\cata\HOG\HOG.srcs\sim_1\new\input_file\gray_normalised.txt";
 begin
     for row in 0 to IMG_HEIGHT - 1 loop
         readline(text_file, row_text);
@@ -121,36 +116,31 @@ begin
     )
     port map(
         clk => clk_s,
-        pix_0 => reg30(0),
-        pix_1 => reg30(1),
-        pix_2 => reg30(2),
-        pix_3 => reg30(3),
-        pix_4 => reg30(4),
-        pix_5 => reg30(5),
-        pix_6 => reg30(6),
-        pix_7 => reg30(7),
-        pix_8 => reg30(8),
-        pix_9 => reg30(9),
-        pix_10 => reg30(10),
-        pix_11 => reg30(11), 
-        pix_12 => reg30(12),
-        pix_13 => reg30(13),
-        pix_14 => reg30(14),
-        pix_15 => reg30(15),
-        pix_16 => reg30(16),
-        pix_17 => reg30(17), 
-        pix_18 => reg30(18),
-        pix_19 => reg30(19),
-        pix_20 => reg30(20),
-        pix_21 => reg30(21),
-        pix_22 => reg30(22),
-        pix_23 => reg30(23),
-        pix_24 => reg30(24),
-        pix_25 => reg30(25),
-        pix_26 => reg30(26),
-        pix_27 => reg30(27),
-        pix_28 => reg30(28),
-        pix_29 => reg30(29),   
+        pix_0 => reg24(0),
+        pix_1 => reg24(1),
+        pix_2 => reg24(2),
+        pix_3 => reg24(3),
+        pix_4 => reg24(4),
+        pix_5 => reg24(5),
+        pix_6 => reg24(6),
+        pix_7 => reg24(7),
+        pix_8 => reg24(8),
+        pix_9 => reg24(9),
+        pix_10 => reg24(10),
+        pix_11 => reg24(11), 
+        pix_12 => reg24(12),
+        pix_13 => reg24(13),
+        pix_14 => reg24(14),
+        pix_15 => reg24(15),
+        pix_16 => reg24(16),
+        pix_17 => reg24(17), 
+        pix_18 => reg24(18),
+        pix_19 => reg24(19),
+        pix_20 => reg24(20),
+        pix_21 => reg24(21),
+        pix_22 => reg24(22),
+        pix_23 => reg24(23),
+  
         res_x_0 => dram(0),
         res_x_1 => dram(1),
         res_x_2 => dram(2),
@@ -175,44 +165,37 @@ begin
         --for i in 0 to 0 loop
             --for j in 0 to 10 loop
                 --for k in 0 to 9 loop
-                    --reg30(0 + k*3) <= bram(i*9 + k)(j)      after (i*149+ j)*100 ns;
-                    --reg30(1 + k*3) <= bram(i*9 + k)(j + 1)    after (i*149+ j)*100 ns;
-                    --reg30(2 + k*3) <= bram(i*9 + k)(j + 2)    after (i*149+ j)*100 ns;
+                    --reg24(0 + k*3) <= bram(i*9 + k)(j)      after (i*149+ j)*100 ns;
+                    --reg24(1 + k*3) <= bram(i*9 + k)(j + 1)    after (i*149+ j)*100 ns;
+                    --reg24(2 + k*3) <= bram(i*9 + k)(j + 2)    after (i*149+ j)*100 ns;
                 --end loop;
             --end loop;
         --end loop;
         --wait;
-        reg30(0) <= bram(0)(0), bram(0)(1) after 100 ns, bram(0)(2) after 200 ns;
-        reg30(1) <= bram(0)(1), bram(0)(2) after 100 ns, bram(0)(3) after 200 ns;
-        reg30(2) <= bram(0)(2), bram(0)(3) after 100 ns, bram(0)(4) after 200 ns;
-        reg30(3) <= bram(1)(0), bram(1)(1) after 100 ns, bram(1)(2) after 200 ns;
-        reg30(4) <= bram(1)(1), bram(1)(2) after 100 ns, bram(1)(3) after 200 ns;
-        reg30(5) <= bram(1)(2), bram(1)(3) after 100 ns, bram(1)(4) after 200 ns;
-        reg30(6) <= bram(2)(0), bram(2)(1) after 100 ns, bram(2)(2) after 200 ns;
-        reg30(7) <= bram(2)(1), bram(2)(2) after 100 ns, bram(2)(3) after 200 ns;
-        reg30(8) <= bram(2)(2), bram(2)(3) after 100 ns, bram(2)(4) after 200 ns;
-        reg30(9) <= bram(3)(0), bram(3)(1) after 100 ns, bram(3)(2) after 200 ns;
-        reg30(10) <= bram(3)(1), bram(3)(2) after 100 ns, bram(3)(3) after 200 ns;
-        reg30(11) <= bram(3)(2), bram(3)(3) after 100 ns, bram(3)(4) after 200 ns;        
-        reg30(12) <= bram(4)(0), bram(4)(1) after 100 ns, bram(4)(2) after 200 ns;
-        reg30(13) <= bram(4)(1), bram(4)(2) after 100 ns, bram(4)(3) after 200 ns;
-        reg30(14) <= bram(4)(2), bram(4)(3) after 100 ns, bram(4)(4) after 200 ns;
-        reg30(15) <= bram(5)(0), bram(5)(1) after 100 ns, bram(5)(2) after 200 ns;
-        reg30(16) <= bram(5)(1), bram(5)(2) after 100 ns, bram(5)(3) after 200 ns;
-        reg30(17) <= bram(5)(2), bram(5)(3) after 100 ns, bram(5)(4) after 200 ns;
-        reg30(18) <= bram(6)(0), bram(6)(1) after 100 ns, bram(6)(2) after 200 ns;
-        reg30(19) <= bram(6)(1), bram(6)(2) after 100 ns, bram(6)(3) after 200 ns;
-        reg30(20) <= bram(6)(2), bram(6)(3) after 100 ns, bram(6)(4) after 200 ns;
-        reg30(21) <= bram(7)(0), bram(7)(1) after 100 ns, bram(7)(2) after 200 ns;
-        reg30(22) <= bram(7)(1), bram(7)(2) after 100 ns, bram(7)(3) after 200 ns;
-        reg30(23) <= bram(7)(2), bram(7)(3) after 100 ns, bram(7)(4) after 200 ns;
-        reg30(24) <= bram(8)(0), bram(8)(1) after 100 ns, bram(8)(2) after 200 ns;
-        reg30(25) <= bram(8)(1), bram(8)(2) after 100 ns, bram(8)(3) after 200 ns;
-        reg30(26) <= bram(8)(2), bram(8)(3) after 100 ns, bram(8)(4) after 200 ns;        
-        reg30(27) <= bram(9)(0), bram(9)(1) after 100 ns, bram(9)(2) after 200 ns;
-        reg30(28) <= bram(9)(1), bram(9)(2) after 100 ns, bram(9)(3) after 200 ns;
-        reg30(29) <= bram(9)(2), bram(9)(3) after 100 ns, bram(9)(4) after 200 ns;
-        
+        reg24(0) <= bram(0)(0), bram(0)(1) after 100 ns, bram(0)(2) after 200 ns;
+        reg24(1) <= bram(0)(1), bram(0)(2) after 100 ns, bram(0)(3) after 200 ns;
+        reg24(2) <= bram(0)(2), bram(0)(3) after 100 ns, bram(0)(4) after 200 ns;
+        reg24(3) <= bram(0)(3), bram(0)(4) after 100 ns, bram(0)(5) after 200 ns;
+        reg24(4) <= bram(1)(0), bram(1)(1) after 100 ns, bram(1)(2) after 200 ns;
+        reg24(5) <= bram(1)(1), bram(1)(2) after 100 ns, bram(1)(3) after 200 ns;
+        reg24(6) <= bram(1)(2), bram(1)(3) after 100 ns, bram(1)(4) after 200 ns;
+        reg24(7) <= bram(1)(3), bram(1)(4) after 100 ns, bram(1)(5) after 200 ns;      
+        reg24(8) <= bram(2)(0), bram(2)(1) after 100 ns, bram(2)(2) after 200 ns;
+        reg24(9) <= bram(2)(1), bram(2)(2) after 100 ns, bram(2)(3) after 200 ns;
+        reg24(10) <= bram(2)(2), bram(2)(3) after 100 ns, bram(2)(4) after 200 ns;
+        reg24(11) <= bram(2)(3), bram(2)(4) after 100 ns, bram(2)(5) after 200 ns;
+        reg24(12) <= bram(3)(0), bram(3)(1) after 100 ns, bram(3)(2) after 200 ns;
+        reg24(13) <= bram(3)(1), bram(3)(2) after 100 ns, bram(3)(3) after 200 ns;
+        reg24(14) <= bram(3)(2), bram(3)(3) after 100 ns, bram(3)(4) after 200 ns;
+        reg24(15) <= bram(3)(3), bram(3)(4) after 100 ns, bram(3)(5) after 200 ns; 
+        reg24(16) <= bram(4)(0), bram(4)(1) after 100 ns, bram(4)(2) after 200 ns;
+        reg24(17) <= bram(4)(1), bram(4)(2) after 100 ns, bram(4)(3) after 200 ns;
+        reg24(18) <= bram(4)(2), bram(4)(3) after 100 ns, bram(4)(4) after 200 ns;
+        reg24(19) <= bram(4)(3), bram(4)(4) after 100 ns, bram(4)(5) after 200 ns;
+        reg24(20) <= bram(5)(0), bram(5)(1) after 100 ns, bram(5)(2) after 200 ns;
+        reg24(21) <= bram(5)(1), bram(5)(2) after 100 ns, bram(5)(3) after 200 ns;
+        reg24(22) <= bram(5)(2), bram(5)(3) after 100 ns, bram(5)(4) after 200 ns;
+        reg24(23) <= bram(5)(3), bram(5)(4) after 100 ns, bram(5)(5) after 200 ns;         
         
        wait;
     end process;
