e67e827b504ce8c5ac4dd02ea5db7b95bff79b83
Andjela <andjelamokvica@gmail.com>
2024-08-24 19:11:53 +0200

    connected system

--- RTL/tb/tb_top.vhd
+++ RTL/tb/tb_top.vhd
@@ -50,14 +50,14 @@ component top is
     width_4: in std_logic_vector(7 downto 0);
     width_2: in std_logic_vector(8 downto 0);
     height: in std_logic_vector(10 downto 0);
-    bram_height: in std_logic_vector(3 downto 0);
+    bram_height: in std_logic_vector(4 downto 0);
     dram_in_addr: in std_logic_vector(31 downto 0);
     dram_x_addr: in std_logic_vector(31 downto 0);
     dram_y_addr: in std_logic_vector(31 downto 0);
     cycle_num_limit: in std_logic_vector(5 downto 0); --2*bram_width/width
     cycle_num_out: in std_logic_vector(5 downto 0); --2*(bram_width/(width-1))
     rows_num: in std_logic_vector(9 downto 0); --2*(bram_width/width)*bram_height
-    effective_row_limit: in std_logic_vector(9 downto 0); --(height/PTS_PER_ROW)/accumulated_loss
+    effective_row_limit: in std_logic_vector(11 downto 0); --(height/PTS_PER_COL)*PTS_PER_COL+accumulated_loss 
     
     burst_len_read: out std_logic_vector(7 downto 0);
     burst_len_write: out std_logic_vector(7 downto 0);
@@ -87,14 +87,14 @@ signal width_s: std_logic_vector(9 downto 0);
 signal width_4_s: std_logic_vector(7 downto 0);
 signal width_2_s: std_logic_vector(8 downto 0);
 signal height_s: std_logic_vector(10 downto 0);
-signal bram_height_s: std_logic_vector(3 downto 0);
+signal bram_height_s: std_logic_vector(4 downto 0);
 signal dram_in_addr_s: std_logic_vector(31 downto 0);
-signal dram_x_addr_s: std_logic_vector(31 downto 0);
-signal dram_y_addr_s: std_logic_vector(31 downto 0);
+signal dram_x_addr_s: std_logic_vector(31 downto 0); --height*width
+signal dram_y_addr_s: std_logic_vector(31 downto 0); --height*width+(height-2)*(width-2)
 signal cycle_num_limit_s: std_logic_vector(5 downto 0); --2*bram_width/width
 signal cycle_num_out_s: std_logic_vector(5 downto 0); --2*(bram_width/(width-1))
 signal rows_num_s: std_logic_vector(9 downto 0); --2*(bram_width/width)*bram_height
-signal effective_row_limit_s: std_logic_vector(9 downto 0); --(height/PTS_PER_ROW)/accumulated_loss 
+signal effective_row_limit_s: std_logic_vector(11 downto 0); --(height/PTS_PER_COL)*PTS_PER_COL+accumulated_loss 
 
 signal burst_len_read_s: std_logic_vector(7 downto 0);
 signal burst_len_write_s: std_logic_vector(7 downto 0);
@@ -189,24 +189,33 @@ port map(
         data_out2 => data_out_s(1));
         
     stim_gen1: process
+        variable a : integer := 0;
+        variable b: integer := 0;
     begin
-        data_in_s(0) <= dram(0)(0)&dram(0)(1)&dram(0)(2)&dram(0)(3), dram(2)(0)&dram(2)(1)&dram(2)(2)&dram(2)(3) after 15ns, dram(4)(0)&dram(4)(1)&dram(4)(2)&dram(4)(3) after 30ns; 
-        data_in_s(1) <= dram(1)(0)&dram(1)(1)&dram(1)(2)&dram(1)(3), dram(3)(0)&dram(3)(1)&dram(3)(2)&dram(3)(3) after 15ns, dram(5)(0)&dram(5)(1)&dram(5)(2)&dram(5)(3) after 30ns; 
         reset_s <= '1', '0' after 55ns;
-        start_s <= '1', '0' after 110ns;
-        en_axi_s <= '1';
-        width_s <= "0000000100";
-        width_4_s <= "00000001";
-        width_2_s <= "000000010";
-        height_s <= "00000000110";
-        bram_height_s <= "1111"; --provjeri visinu
-        dram_in_addr_s <= x"00000000";
-        dram_x_addr_s <= x"00000000";
-        dram_y_addr_s <= x"00000000";
-        cycle_num_limit_s <= (others => '0');
-        cycle_num_out_s <= (others => '0');
-        rows_num_s <= (others => '0');
-        effective_row_limit_s <= (others => '0');
+        start_s <= '1', '0' after 175ns;
+        --data_in_s(0) <= dram(0)(0)&dram(0)(1)&dram(0)(2)&dram(0)(3), dram(2)(0)&dram(2)(1)&dram(2)(2)&dram(2)(3) after 15ns, dram(4)(0)&dram(4)(1)&dram(4)(2)&dram(4)(3) after 30ns; 
+        --data_in_s(1) <= dram(1)(0)&dram(1)(1)&dram(1)(2)&dram(1)(3), dram(3)(0)&dram(3)(1)&dram(3)(2)&dram(3)(3) after 15ns, dram(5)(0)&dram(5)(1)&dram(5)(2)&dram(5)(3) after 30ns; 
+        for a in 0 to 75 loop
+            for b in 0 to 37 loop
+                data_in_s(0) <= dram(2*a)(4*b)&dram(2*a)(4*b+1)&dram(2*a)(4*b+2)&dram(2*a)(4*b+3); 
+                data_in_s(1) <= dram(2*a+1)(4*b)&dram(2*a+1)(4*b+1)&dram(2*a+1)(4*b+2)&dram(2*a+1)(4*b+3);
+                en_axi_s <= '1';
+                width_s <= "0010011000";
+                width_4_s <= "00100110";
+                width_2_s <= "001001100";
+                height_s <= "00001001100";
+                bram_height_s <= "10000"; 
+                dram_in_addr_s <= x"00000000";
+                dram_x_addr_s <= x"00005A40";
+                dram_y_addr_s <= x"0000B350";
+                cycle_num_limit_s <= "001101";
+                cycle_num_out_s <= "001101";
+                rows_num_s <= "0011010111";
+                effective_row_limit_s <= "000010011000";
+            wait until falling_edge(clk_s);
+            end loop;
+        end loop;       
         
     wait;
     end process;
